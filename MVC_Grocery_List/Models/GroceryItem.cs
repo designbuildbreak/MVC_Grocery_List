6d6f94dd3b9634aaf63de9897d46fbcd1fda69c2
Aaron Boesch <ajboesch@gmail.com>
2015-06-10 06:23:39 -0400

    initial commit

---
+++ MVC_Grocery_List/Models/GroceryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MVC_Grocery_List.Models
+{
+    public class GroceryItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FoodGroup { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class GroceryListDBContext : DbContext
+    {
+        public DbSet<GroceryItem> GroceryItems { get; set; } //Looks like the database table is named GroceryItems.mdf
+    }
+}
